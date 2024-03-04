8b63129f8a289e65b3ad180f91c53f61ce763fc2
peerapon.ph <peerapon.ph@kkumail.com>
2024-03-05 00:18:21 +0700

    back to  profile

--- work6/work6.js
+++ work6/work6.js
@@ -12,61 +12,68 @@ class App extends React.Component {
     footer = (
         <div>
             By 643021113-0 Peerapon Phokum <br />
-            College of Computing, Khon Kaen University
+            College of Computing, Khon Kaen University <br />
+            <Button onClick={() => this.goBack()}>Back to Profile</Button>
         </div>
     );
     state = {
         scene: 0,
-        students:[],
-        stdid:"",
-        stdtitle:"",
-        stdfname:"",
-        stdlname:"",
-        stdemail:"",
-        stdphone:"",
+        students: [],
+        stdid: "",
+        stdtitle: "",
+        stdfname: "",
+        stdlname: "",
+        stdemail: "",
+        stdphone: "",
+        link: 'https://peerapon3014.github.io/web-2566/',
     }
 
-    edit(std){      
+    goBack(){
+        window.location.href = this.state.link;
+    }
+
+    edit(std) {
         this.setState({
-         stdid    : std.id,
-         stdtitle : std.title,
-         stdfname : std.fname,
-         stdlname : std.lname,
-         stdemail : std.email,
-         stdphone : std.phone,
+            stdid: std.id,
+            stdtitle: std.title,
+            stdfname: std.fname,
+            stdlname: std.lname,
+            stdemail: std.email,
+            stdphone: std.phone,
         })
-     }
-     delete(std){
-        if(confirm("ต้องการลบข้อมูล")){
-           db.collection("students").doc(std.id).delete();
+    }
+    
+    delete(std) {
+        if (confirm("ต้องการลบข้อมูล")) {
+            db.collection("students").doc(std.id).delete();
         }
     }
 
- 
+
     render() {
         return (
             <Card>
-              <Card.Header>{this.title}</Card.Header>  
-              <Card.Body>
-                <Button onClick={()=>this.readData()}>Read Data</Button>
-                <Button onClick={()=>this.autoRead()}>Auto Read</Button>
-                <div>
-                <StudentTable data={this.state.students} app={this}/>  
-                </div>
-              </Card.Body>
-              <Card.Footer>
-              <b>เพิ่ม/แก้ไขข้อมูล นักศึกษา :</b><br/>
-              <TextInput label="ID" app={this} value="stdid" style={{width:120}}/>  
-              <TextInput label="คำนำหน้า" app={this} value="stdtitle" style={{width:100}} />
-              <TextInput label="ชื่อ" app={this} value="stdfname" style={{width:120}}/>
-              <TextInput label="สกุล" app={this} value="stdlname" style={{width:120}}/>
-              <TextInput label="Email" app={this} value="stdemail" style={{width:150}} />        
-              <TextInput label="Phone" app={this} value="stdphone" style={{width:120}}/>
-              <Button onClick={()=>this.insertData()}>Save</Button>
-              </Card.Footer>
-              <Card.Footer>{this.footer}</Card.Footer>
-            </Card>          
-          );    
+                <Card.Header>{this.title}</Card.Header>
+                <Card.Body>
+                    <Button onClick={() => this.readData()}>Read Data</Button>
+                    <Button onClick={() => this.autoRead()}>Auto Read</Button>
+                    <div>
+                        <StudentTable data={this.state.students} app={this} />
+                    </div>
+                </Card.Body>
+                <Card.Footer>
+                    <b>เพิ่ม/แก้ไขข้อมูล นักศึกษา :</b><br />
+                    <TextInput label="ID" app={this} value="stdid" style={{ width: 120 }} />
+                    <TextInput label="คำนำหน้า" app={this} value="stdtitle" style={{ width: 100 }} />
+                    <TextInput label="ชื่อ" app={this} value="stdfname" style={{ width: 120 }} />
+                    <TextInput label="สกุล" app={this} value="stdlname" style={{ width: 120 }} />
+                    <TextInput label="Email" app={this} value="stdemail" style={{ width: 150 }} />
+                    <TextInput label="Phone" app={this} value="stdphone" style={{ width: 120 }} />
+                    <Button onClick={() => this.insertData()}>Save</Button>
+                </Card.Footer>
+                <Card.Footer>{this.footer}</Card.Footer>
+            </Card>
+        );
     }
     readData() {
         db.collection("students").get().then((querySnapshot) => {
@@ -79,23 +86,23 @@ class App extends React.Component {
         });
     }
 
-    autoRead(){
+    autoRead() {
         db.collection("students").onSnapshot((querySnapshot) => {
-            var stdlist=[];
+            var stdlist = [];
             querySnapshot.forEach((doc) => {
-                stdlist.push({id:doc.id,... doc.data()});                
-            });          
-            this.setState({students: stdlist});
+                stdlist.push({ id: doc.id, ...doc.data() });
+            });
+            this.setState({ students: stdlist });
         });
     }
-    
-    insertData(){
+
+    insertData() {
         db.collection("students").doc(this.state.stdid).set({
-           title : this.state.stdtitle,
-           fname : this.state.stdfname,
-           lname : this.state.stdlname,
-           phone : this.state.stdphone,
-           email : this.state.stdemail,
+            title: this.state.stdtitle,
+            fname: this.state.stdfname,
+            lname: this.state.stdlname,
+            phone: this.state.stdphone,
+            email: this.state.stdemail,
         });
     }
 
@@ -127,45 +134,46 @@ db.collection("students").get().then((querySnapshot) => {
     });
 });
 
-function StudentTable({data, app}){
+function StudentTable({ data, app }) {
     return <table className='table'>
-     <tr>
-         <td>รหัส</td>
-         <td>คำนำหน้า</td>
-         <td>ชื่อ</td>
-         <td>สกุล</td>
-         <td>email</td>
-         </tr>
-         {
-           data.map((s)=><tr>
-           <td>{s.id}</td>
-           <td>{s.title}</td>
-           <td>{s.fname}</td>
-           <td>{s.lname}</td>
-           <td>{s.email}</td>        
-           <td><EditButton std={s} app={app}/></td>   
-           <td><DeleteButton std={s} app={app}/></td> 
-           </tr> )
-         }
-     </table>
-   }
- 
-   function TextInput({label,app,value,style}){
-    return <label className="form-label">
-    {label}:    
-     <input className="form-control" style={style}
-     value={app.state[value]} onChange={(ev)=>{
-         var s={};
-         s[value]=ev.target.value;
-         app.setState(s)}
-     }></input>
-   </label>;  
-  }
+        <tr>
+            <td>รหัส</td>
+            <td>คำนำหน้า</td>
+            <td>ชื่อ</td>
+            <td>สกุล</td>
+            <td>email</td>
+        </tr>
+        {
+            data.map((s) => <tr>
+                <td>{s.id}</td>
+                <td>{s.title}</td>
+                <td>{s.fname}</td>
+                <td>{s.lname}</td>
+                <td>{s.email}</td>
+                <td><EditButton std={s} app={app} /></td>
+                <td><DeleteButton std={s} app={app} /></td>
+            </tr>)
+        }
+    </table>
+}
 
-  function EditButton({std,app}){
-    return <button onClick={()=>app.edit(std)}>แก้ไข</button>
-   }
- 
-   function DeleteButton({std,app}){    
-    return <button onClick={()=>app.delete(std)}>ลบ</button>
-  }
+function TextInput({ label, app, value, style }) {
+    return <label className="form-label">
+        {label}:
+        <input className="form-control" style={style}
+            value={app.state[value]} onChange={(ev) => {
+                var s = {};
+                s[value] = ev.target.value;
+                app.setState(s)
+            }
+            }></input>
+    </label>;
+}
+
+function EditButton({ std, app }) {
+    return <button onClick={() => app.edit(std)}>แก้ไข</button>
+}
+
+function DeleteButton({ std, app }) {
+    return <button onClick={() => app.delete(std)}>ลบ</button>
+}
